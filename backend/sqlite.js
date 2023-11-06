ce83911cc74462f2fa89883001c6c22f81696daf
tharindu8899 <tharindud8899@gmail.com>
2023-11-06 06:38:06 +0530

    task completed18

--- backend/sqlite.js
+++ backend/sqlite.js
@@ -3,24 +3,47 @@ const sqlite3 = require('sqlite3').verbose();
 let _DBConnection;
 
 const connectDatabase = async () => {
-
-  if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "test-backend") {
+  try {
+    if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "test-backend") {
       return new sqlite3.Database(":memory:", sqlite3.OPEN_READWRITE);
   } else {
     return new sqlite3.Database('./db.sqlite', sqlite3.OPEN_READWRITE);
   }
-}
+
+  } catch (error) {
+    //Handle database connection error
+    console.error("Database connection error: " + error.message);
+  }
+
+  
+};
 
 const getDbConnection = async () => {
-  if (!_DBConnection) {
+  try {
+    if (!_DBConnection) {
     _DBConnection = await connectDatabase();
   }
   return _DBConnection;
+  }catch (error) {
+    //Handle error when getting a database connection
+    console.error("error while getting database connection: " + error.message);
+    throw error;
+  }
+  
 };
 
 const closeConnection = conn => {
   if (conn) {
-    return conn.close();
+    try {
+      conn.close((error) => {
+        if (error) {
+          console.error("Error while closing database connection: " + error.message);
+        }
+      });
+    }catch (error) {
+      console.error("Error while closing database connection: " + error.message);
+    }
+    //return conn.close();
   }
 };
 
